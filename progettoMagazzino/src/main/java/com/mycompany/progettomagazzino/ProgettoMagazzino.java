bcb37cdcb59c4cf74efd921c313ae9a2dacfdf61
Davide <daviderichini@gmail.com>
2024-04-29 22:17:13 +0200

    create poject

---
+++ progettoMagazzino/src/main/java/com/mycompany/progettomagazzino/ProgettoMagazzino.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progettomagazzino;
+
+/**
+ *
+ * @author 39350
+ */
+public class ProgettoMagazzino {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
